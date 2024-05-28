05397eb9b236fcf582eac3af8d36d95af2b0bb52
Filippo-Angaroni <angaronifilippo@gmail.com>
2024-05-28 15:48:15 +0200

    update Events.js, Home.css, index.html and root.css

--- Events.js
+++ Events.js
@@ -1,18 +1,9 @@
 
 window.addEventListener("scroll", windowScrollEvent, false);
 
-var isbg = true;
-
 function windowScrollEvent() {
-    var element = document.getElementById("homePageHeader");
-    var target = document.getElementById("menuHeaderBackground");
-    var rect = element.getBoundingClientRect();
-    var elementBottom = rect.height + rect.y;
-    console.log(`path(${MenuHeaderBackgroundClippingPath(20, 500, 100, window.innerWidth)})`)
-    if (isbg && elementBottom < 0 && !target.classList.contains('visible')) {
-        target.classList.add('visible');
-        //target.style.clipPath = `path(\"${MenuHeaderBackgroundClippingPath(20, 550, 100, window.innerWidth)}\")`;
-        isbg = false;
-        console.log(1);
-    }
+    var titleDiv = document.getElementById("homePageHeaderTitleDiv");
+    titleDiv.style.paddingLeft = "0px";
+    titleDiv.style.marginLeft = "-" + titleDiv.offsetWidth + "px";
+    console.log("-" + titleDiv.offsetWidth + "px");
 }
